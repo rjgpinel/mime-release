412052da8c4ba8abe51cd56a635149219fd920b3
rgarciap <rjgpinel@gmail.com>
2022-01-29 15:07:47 +0100

    table_envs/tower_scene: clean code

--- mime/envs/table_envs/tower_scene.py
+++ mime/envs/table_envs/tower_scene.py
@@ -16,10 +16,7 @@ class TowerScene(TableScene):
         self._cubes = []
         self._cubes_size = []
 
-        if self._rand_obj:
-            self._cube_size_range = {"low": 0.04, "high": 0.06}
-        else:
-            self._cube_sizes = [0.05 + i * 0.01 for i in range(self._num_cubes)]
+        self._cube_size_range = {"low": 0.04, "high": 0.06}
 
         v, w = self._max_tool_velocity
         self._max_tool_velocity = (1.5 * v, w)
@@ -39,14 +36,13 @@ class TowerScene(TableScene):
         self._count_success = 0
 
         low, high = self._object_workspace
-        low_arm, high_arm = low.copy(), high.copy()
-        low_arm[2] = 0.1
         low_cubes, high_cubes = np.array(low.copy()), np.array(high.copy())
-        low_cubes[:2] += 0.02
-        high_cubes[:2] -= 0.02
+        low_cubes[:2] += self._cube_size_range["high"] / 2
+        high_cubes[:2] -= self._cube_size_range["high"] / 2
 
         for cube in self._cubes:
             cube.remove()
+
         self._cubes = []
         self._cubes_size = []
 
@@ -60,15 +56,7 @@ class TowerScene(TableScene):
         cubes = []
         cubes_size = []
         for i in range(num_cubes):
-            if self._rand_obj:
-                cube, cube_size = modder.load_mesh(
-                    "cube", self._cube_size_range, np_random
-                )
-            else:
-                cube, cube_size = modder.load_mesh(
-                    "cube", self._cube_sizes[i], np_random
-                )
-            # cube.dynamics.lateral_friction = 10
+            cube, cube_size = modder.load_mesh("cube", self._cube_size_range, np_random)
             cubes.append(cube)
             cubes_size.append(cube_size)
         # sort cubes per decreasing size
@@ -87,10 +75,7 @@ class TowerScene(TableScene):
                 cube, aabbs, np_random, low_cubes, high_cubes, 0, 0, min_dist=0.05
             )
 
-            if self._rand_obj:
-                color = np_random.uniform(0, 1, 4)
-            else:
-                color = np.array([11.0 / 255.0, 124.0 / 255.0, 96.0 / 255.0, 1])
+            color = np.array([11.0 / 255.0, 124.0 / 255.0, 96.0 / 255.0, 1])
             color[3] = 1
             cube.color = color
             if self._domain_rand:
